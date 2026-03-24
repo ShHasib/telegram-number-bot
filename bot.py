d40be13d91d1de58bfaeb2ce3ec42b53348de1d5
ShHasib <106856292+ShHasib@users.noreply.github.com>
2026-03-24 12:53:12 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,28 +1,30 @@
-import re
+ import re
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
-used_numbers = set()
+used_numbers = {}
 
 async def check_number(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = update.message.text
+    user = update.effective_user
 
-    # নাম্বার খুঁজবে যেকোনো দেশের জন্য
     numbers = re.findall(r'\+?\d{7,15}', text)
 
     if numbers:
         number = numbers[0]
 
+        username = user.username if user.username else user.first_name
+
         if number in used_numbers:
-            await update.message.reply_text("❌ Already Used")
+            used_by = used_numbers[number]
+            await update.message.reply_text(f"❌ Already Used by: @{used_by}")
         else:
-            used_numbers.add(number)
+            used_numbers[number] = username
             await update.message.reply_text("✅ Available")
 
-# Bot Token বসাও
 app = ApplicationBuilder().token("8215918965:AAGn83KeyVMYlPN-cGXEtnLmvmqNUNBT_UY").build()
 
-# সব টেক্সট মেসেজ চেক করবে
 app.add_handler(MessageHandler(filters.TEXT, check_number))
 
+print("Bot Running...")
 app.run_polling()
